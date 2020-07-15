d3eb9998a3c665ec1be00c8005b511e76b6fe7a4
Mohammed El-khamisi <mohammd.alkhamisi@gmail.com>
2020-07-16 00:16:05 +0200

    Update Interpolation_Search.cpp

--- Searching_Algorithms/Interpolation_Search.cpp
+++ Searching_Algorithms/Interpolation_Search.cpp
@@ -8,7 +8,7 @@ int Interpolation_Search(int arr[],int n,int key);
 
 int main() {
 
-  int X[]={1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49}; // Array Of uniformly distributed Values
+  int X[]={1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49}; // Array Of uniformly distributed Values must be Sorted
   
   
     cout<<"What Are You Looking For: ";
@@ -19,9 +19,9 @@ int main() {
   int Ans= Interpolation_Search(X,sizeof(X)/sizeof(X[0]),value);
   
     if (Ans != -1)
-      cout<< " The Value is Exists "<<Ans;
+      cout<< " The Value does Exists "<<Ans;
     else
-        cout<< "The Value is not Exists";
+        cout<< "The Value doesn't Exists";
     
 
 
